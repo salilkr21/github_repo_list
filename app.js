61222b7a0cad3d7903f1c81b3267396a28f66417
salil <salilkumar21@gmail.com>
2021-08-23 19:22:53 +0530

    routes folder made and route is defined inside it

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-const express= require('express');
 require("dotenv").config();
 const fetch = require("node-fetch");
-const app = express();
+const app = require('./routes/api_route');
 
-app.get('/:username', (req,res)=>
-{
-    var user_name = req.params.username;
-    var url = 'https://api.github.com/users/' + user_name+ '/repos';
 
-    fetch(url).then(response => response.json()).then((data) =>
-    {
-    for(i=0;i<data.length;i++)
-    {
-        res.write((i+1)+" "+(data[i].name)+"\n");
-        
-    }
-    res.send();
-    }).catch(error => console.log(error));
-
-});
-
-app.listen(process.env.APP_PORT,'127.0.0.1',()=>
-{
+app.listen(process.env.APP_PORT, '127.0.0.1', () => {
     console.log(`listening to port ${process.env.APP_PORT}`)
 });
